447d27f6a7f9345ad55e234cb76a5e5ec909c571
stevensotelo <stevenbetancurt@hotmail.com>
2016-09-21 14:51:55 -0500

    Init project with scalfolding for the website

---
+++ website/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name globalDietApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the globalDietApp
+ */
+angular.module('globalDietApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
