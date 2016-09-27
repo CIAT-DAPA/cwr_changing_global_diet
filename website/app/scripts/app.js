00e5b81bf35dc630e5933f566d1a3bb9c4d210f0
stevensotelo <stevenbetancurt@hotmail.com>
2016-09-27 15:41:57 -0500

    scalfolding of the style added

--- website/app/scripts/app.js
+++ website/app/scripts/app.js
@@ -8,32 +8,7 @@
  *
  * Main module of the application.
  */
-angular
-  .module('globalDietApp', [
-    'ngCookies',
-    'ngRoute'
-  ])
-  .value('config',{
-      global_diet_tsv: 'data/global_diet_tsv.tsv'
-  })
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/flowingdata', {
-        templateUrl: 'views/flowingdata.html',
-        controller: 'FlowingdataCtrl',
-        controllerAs: 'cflowingdata'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('globalDietApp', [
+        'swipe',
+        'snapscroll'
+      ]);
